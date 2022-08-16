7db921b83d199da11b9ecf50c684f68bb2f100b0
Pony_Yang <Pony_Yang@compal.com>
2022-08-16 19:30:02 +0800

    GPIO dump version 1.

--- Application/GpioRplH/GpioRplH.c
+++ Application/GpioRplH/GpioRplH.c
@@ -3,20 +3,147 @@
 #include <Library/ShellCEntryLib.h>
 #include "GpioRplH.h"
 
-INTN
-EFIAPI
+VOID GPIO_Info (UINTN config)
+{
+
+  UINTN   GpioNum = 0;
+  UINTN   GpioAddr = 0;
+  UINT8   PAD_MODE = 0;
+  UINT8   Reset = 0;
+  CHAR8   *GpioData = NULL; 
+  BOOLEAN GPI = FALSE;
+  BOOLEAN GPO = FALSE; 
+  UINTN   GPIO_NUM;
+  UINTN   GPIO_BASE;
+  UINTN   Community;
+  CHAR16 *GPIO_Group;
+  
+  switch(config) {
+    case 0:
+      GPIO_BASE = GPP_A_BASE;
+      GPIO_NUM = GPP_A_NUM;
+      Community = COMMUNITY0;
+      GPIO_Group = L"GPP_A";
+      break;
+    case 1:
+      GPIO_BASE = GPP_B_BASE;
+      GPIO_NUM = GPP_B_NUM;
+      Community = COMMUNITY0;
+      GPIO_Group = L"GPP_B";
+      break;
+    case 2:
+      GPIO_BASE = GPP_C_BASE;
+      GPIO_NUM = GPP_C_NUM;
+      Community = COMMUNITY4;
+      GPIO_Group = L"GPP_C";
+      break;
+    case 3:
+      GPIO_BASE = GPP_D_BASE;
+      GPIO_NUM = GPP_D_NUM;
+      Community = COMMUNITY1;
+      GPIO_Group = L"GPP_D";
+      break;
+    case 4:
+      GPIO_BASE = GPP_E_BASE;
+      GPIO_NUM = GPP_E_NUM;
+      Community = COMMUNITY4;
+      GPIO_Group = L"GPP_E";
+      break;
+    case 5:
+      GPIO_BASE = GPP_F_BASE;
+      GPIO_NUM = GPP_F_NUM;
+      Community = COMMUNITY4;
+      GPIO_Group = L"GPP_F";
+      break;
+    case 6:
+      GPIO_BASE = GPP_H_BASE;
+      GPIO_NUM = GPP_H_NUM;
+      Community = COMMUNITY1;
+      GPIO_Group = L"GPP_H";
+      break;
+    case 7:
+      GPIO_BASE = GPP_R_BASE;
+      GPIO_NUM = GPP_R_NUM;
+      Community = COMMUNITY5;
+      GPIO_Group = L"GPP_R";
+      break;
+    case 8:
+      GPIO_BASE = GPP_S_BASE;
+      GPIO_NUM = GPP_S_NUM;
+      Community = COMMUNITY1;
+      GPIO_Group = L"GPP_S";
+      break;
+    case 9:
+      GPIO_BASE = GPP_T_BASE;
+      GPIO_NUM = GPP_T_NUM;
+      Community = COMMUNITY0;
+      GPIO_Group = L"GPP_T";
+      break;
+    default:
+      GPIO_BASE = GPD_BASE;
+      GPIO_NUM = GPD_NUM;
+      Community = COMMUNITY2;
+      GPIO_Group = L"GPD";
+      break;
+  }
+
+  for(GpioNum = 0; GpioNum < GPIO_NUM; GpioNum++) {
+    GpioData = 0;
+    GpioAddr = GPIO_ADDR(Community, GPIO_BASE, GpioNum);
+    Print(L"%s%02d: ", GPIO_Group, GpioNum);
+    // Print(L" %d, ", (UINT8)*(GpioData + GpioAddr + OFFSET_1));
+    PAD_MODE = ((UINT8)*(GpioData + GpioAddr + OFFSET_1) & GPIO_FUNCTION) >> 2;
+    if(!PAD_MODE) {
+      Print(L"GPIO Mode, ");
+    } else {
+      Print(L"Native Function %d\n", PAD_MODE);
+      continue;
+    }
+    // GPI
+    GPI = (BOOLEAN)((*(GpioData + GpioAddr + OFFSET_1) & RX_DISABLE) >> 1);
+    GPI ? Print(L"GPI Disabled, ") : Print(L"GPI Enabled, ");
+    (BOOLEAN)((*(GpioData + GpioAddr + OFFSET_0) & RX_STATE_BIT) >> 1) ? Print(L"Input High, ") : Print(L"Input Low, ");
+    !GPI && (BOOLEAN)((*(GpioData + GpioAddr + OFFSET_1) & RX_INVERSE) >> 7) ? Print(L"Inversion, ") : Print(L"No Inversion, ");  
+    // GPO
+    GPO = (BOOLEAN)(*(GpioData + GpioAddr + OFFSET_1) & TX_DISABLE);
+    GPO ? Print(L"GPO Disabled, ") : Print(L"GPO Enabled, ");
+    (BOOLEAN)(*(GpioData + GpioAddr + OFFSET_0) & TX_STATE_BIT) ? Print(L"Output High, ") : Print(L"Output Low, ");
+    // Reset
+    Reset = (*(GpioData + GpioAddr + OFFSET_3) & PAD_RESET) >> 6;
+    switch(Reset) {
+      case 0:
+        Print(L"Global Reset or RSMRST#.\n");
+        break;
+      case 1:
+        Print(L"Host deep reset.\n");
+        break;
+      case 2:
+        Print(L"PLTRST#.\n");
+        break;
+      case 3:
+        Print(L"Global Reset.\n");
+        break;
+      default:
+        Print(L"Reset: %d.\n", Reset);
+        break;
+    }
+
+  }
+}
+
+INTN EFIAPI
 ShellAppMain (
   IN UINTN Argc,
   IN CHAR16 **Argv
   )
 {
-  // CHAR8 *Address;
-  UINTN  GpioNum;
-  Print(L"[Community 0]\n");
-  // GPP_B
-  for(GpioNum = 0; GpioNum < GPP_B_NUM; GpioNum++) {
-    Print(L"GPP_B%d: 0x%x\n", GpioNum, SBREG_BAR|(COMMUNITY0<<16)|(GPP_B_BASE+(GpioNum<<4)));
+  UINTN  gpio;
+  UINTN  gpio_num = 11;
+  // GPP_A  GPP_B GPP_C GPP_D GPP_E GPP_F GPP_H GPP_R GPP_S GPP_T GPD => 11 
+  for(gpio = 0; gpio < gpio_num; gpio++) {
+    GPIO_Info(gpio);
+    Print(L"\n");
   }
-  
+ 
   return 0;
 }
--- Application/GpioRplH/GpioRplH.h
+++ Application/GpioRplH/GpioRplH.h
@@ -11,7 +11,88 @@
 #define COMMUNITY4     0x6A
 #define COMMUNITY5     0x69
 
+//
+// Community 0
+//
+// GPP_B0 - GPP_B23
 #define GPP_B_BASE     0x700
 #define GPP_B_NUM      24
+// GPP_T2 GPP_T3
+#define GPP_T_BASE     0x8C0
+#define GPP_T_NUM      2
+// GPP_A0 - GPP_A23
+#define GPP_A_BASE     0x9A0
+#define GPP_A_NUM      24
+
+//
+// Community 1
+//
+// GPP_S0 - GPP_S7
+#define GPP_S_BASE     0x700
+#define GPP_S_NUM      8
+// GPP_H0 - GPP_H23
+#define GPP_H_BASE     0x780
+#define GPP_H_NUM      24
+// GPP_D0 - GPP_D19
+#define GPP_D_BASE     0x900
+#define GPP_D_NUM      20
+
+//
+// Community 2
+//
+// GPD_0 - GPD_11
+#define GPD_BASE       0x700
+#define GPD_NUM        12
+
+//
+// Community 4
+//
+// GPP_C0 - GPP_C23
+#define GPP_C_BASE       0x700
+#define GPP_C_NUM        24
+// GPP_F0 - GPP_F23
+#define GPP_F_BASE       0x880
+#define GPP_F_NUM        24
+// GPP_E0 - GPP_E23
+#define GPP_E_BASE       0xA70
+#define GPP_E_NUM        24
+
+//
+// Community 5
+//
+// GPP_R0 - GPP_R7
+#define GPP_R_BASE       0x700
+#define GPP_R_NUM        8
+
+#define OFFSET_0         0
+#define OFFSET_1         1
+#define OFFSET_2         2
+#define OFFSET_3         3
+#define OFFSET_4         4
+#define OFFSET_5         5
+#define OFFSET_6         6
+#define OFFSET_7         7
+
+// OFFSET_0
+#define TX_STATE_BIT     0x1
+#define RX_STATE_BIT     0x2
+// OFFSET_1
+#define TX_DISABLE       0x1
+#define RX_DISABLE       0x2
+#define GPIO_FUNCTION    0x1C
+// OFFSET_2
+#define GPIO_NMI         0x2
+#define GPIO_SMI         0x4
+#define GPIO_SCI         0x8
+#define GPIO_APIC        0x10
+#define RX_INVERSE       0x80
+// OFFSET_3
+#define RX_LEVEL_EDGE    0x6
+#define PAD_RESET        0xC0
+
+// OFFSET_5
+#define TERMINATION      0x3C
+
+#define GPIO_ADDR(COMMUNITY, GPIO_BASE, GPIO)  SBREG_BAR | (COMMUNITY << 16) | ( GPIO_BASE + (GPIO << 4))
 
 #endif
